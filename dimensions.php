d12ce004e0af9b942556bfa54355a2f433ce1ea5
Nigel Cunningham <nigelc@catalyst-au.net>
2016-03-09 10:11:36 +1100

    WR252334: Rework dimensions to be more testable, add tests.
    Signed-off-by: Nigel Cunningham <nigelc@catalyst-au.net>

--- dimensions.php
+++ dimensions.php
@@ -15,35 +15,49 @@ class dimensions {
 	/**
 	 * Find class instances and populate the array
 	 *
-	 * @return bool
-	 *   Whether an error was encountered in enumerating plugins.
+	 * @return array of strings
+	 *   A list of the names of files containing plugins.
 	 */
 	static public function enumerate_plugins()
 	{
 		$dir = dirname(__FILE__) . '/dimensions';
 
-		self::$dimension_instances = array();
-
 		$list_of_files = scandir($dir);
 		foreach($list_of_files as $index => $entry) {
 			if ($entry == '.' || $entry == '..' || substr($entry, -4) != '.php') {
 				unset($list_of_files[$index]);
-				continue;
 			}
+		}
 
+		return $list_of_files;
+	}
+
+	/**
+	 * Instantiate plugins and populate the array.
+	 */
+	static public function instantiate_plugins()
+	{
+		$list_of_files = self::enumerate_plugins();
+
+		$plugins = array();
+
+		foreach($list_of_files as $index => $entry) {
 			require_once(__DIR__ . '/dimensions/' . $entry);
 
 			$class_name = substr($entry, 0, -4);
 
 			// Check the expected class exists.
-			if (!class_exists($class_name, FALSE)) {
+			if (!class_exists('\local\analytics\dimensions\\' . $class_name, FALSE)) {
 				debugging("Local Analytics: File ${entry} in the dimensions directory doesn't define a class named ${class_name}",
 					DEBUG_DEVELOPER);
 				continue;
 			}
 
-			self::$dimension_instances[$class_name] = new $class_name;
+			$class_name = '\local\analytics\dimensions\\' . $class_name;
+			$plugins[$class_name] = new $class_name;
 		}
-	}
 
+		self::$dimension_instances = $plugins;
+		return $plugins;
+	}
 }
---
+++ tests/local_analytics_dimensions_infrastructure_test.php
@@ -0,0 +1,121 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Analytics tests for dimensions infrastructure.
+ *
+ * @package    local_analytics
+ * @category   test
+ * @copyright  2016 Catalyst IT
+ * @author     Nigel Cunningham
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_analytics;
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once(__DIR__ . '/../dimensions.php');
+
+function should_use_mock_scandir($set = NULL) {
+	static $use_mock_scandir = TRUE;
+
+	if (!is_null($set)) {
+		$use_mock_scandir = $set;
+	}
+
+	return $use_mock_scandir;
+}
+
+/**
+ * Mock scandir function.
+ *
+ * @param string $directory
+ *   The directory to scan.
+ *
+ * @return array
+ *   The list of files found.
+ */
+function scandir($directory) {
+	if (should_use_mock_scandir()) {
+		return array(
+			'.',
+			'..',
+			'foo.text',
+			'bar.php',
+		);
+	} else {
+		return \scandir($directory);
+	}
+}
+
+/**
+ * Class local_analytics_dimensions_infrastructure_testcase
+ */
+class local_analytics_dimensions_infrastructure_testcase extends \advanced_testcase {
+
+	/**
+	 * Setup test data.
+	 */
+	public function setUp() {
+		global $CFG;
+
+		$this->resetAfterTest();
+		$this->setAdminUser();
+
+// 		set_config('location', 'header', 'local_analytics');
+	}
+
+	/**
+	 * Test that enumerate_plugins filters non php files out.
+	 *
+	 * GIVEN the dimensions class
+	 * WHEN its enumerate_plugins function is invoked
+	 * THEN the result should be a list of files in the dimensions directory
+	 * AND the '.' and '..' files should be excluded
+	 * AND files not ending in '.php' should be excluded
+	 *
+	 * @test
+	 */
+	public function enumeratePluginsFiltersScanDirAsExpected() {
+		$actual = dimensions::enumerate_plugins();
+
+		// 3 because it's the fourth element (zero indexed) in the scandir result.
+		$expected = array(3 => 'bar.php');
+
+		$this->assertSame($expected, $actual);
+	}
+
+	/**
+	 * Test that instantiate_plugins can instantiate all plugins.
+	 *
+	 * GIVEN the dimensions class
+	 * WHEN its instantiate_plugins function is invoked
+	 * THEN the result should be an array of plugin instances
+	 *
+	 * @test
+	 */
+	public function instantiatePluginsCanReturnAnArrayOfPlugins() {
+		should_use_mock_scandir(FALSE);
+
+		$actual = dimensions::instantiate_plugins();
+
+		foreach($actual as $name => $plugin) {
+			$this->assertInstanceOf($name, $plugin);
+		}
+	}
+
+}
