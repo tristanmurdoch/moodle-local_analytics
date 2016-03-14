8a51e94546d80d89ff444b20c362f37b921fe06c
Nigel Cunningham <nigelc@catalyst-au.net>
2016-03-15 10:09:36 +1100

    WR252334: Add test for setting options function.
    Signed-off-by: Nigel Cunningham <nigelc@catalyst-au.net>

--- tests/dimensions_test.php
+++ tests/dimensions_test.php
@@ -153,7 +153,8 @@ class local_analytics_dimensions_testcase extends \advanced_testcase
      *
      * (assertDebuggingCalled requires that our test be named test* - it uses the backtrace to decide whether a test is running).
      */
-    public function testInstantiatedPluginsComplainsAboutFaultyPlugin() {
+    public function testInstantiatedPluginsComplainsAboutFaultyPlugin()
+    {
         should_use_mock_scandir(false);
 
         $filename = __DIR__ . '/testdata/faultyplugin.php';
@@ -162,4 +163,50 @@ class local_analytics_dimensions_testcase extends \advanced_testcase
         $result = dimensions::instantiate_plugin($filename, $classname);
         $this->assertDebuggingCalled("Local Analytics: File ${filename} doesn't define a class named ${classname}");
     }
+
+    /**
+     * Test that setting_options correctly returns the setting options for a scope.
+     *
+     * Note that this test uses the real plugins and will need to be updated when new
+     * plugins are added.
+     *
+     * GIVEN a file in the plugins directory not having a class that matches the filename
+     * WHEN the instantiate_plugins function tries to use it
+     * THEN it should generate a debugging message about the issue.
+     *
+     * @test
+     */
+    public function settingOptionsReturnsExpectedValues()
+    {
+        should_use_mock_scandir(false);
+
+        $actual = dimensions::setting_options('visit');
+        $expected = array(
+            '' => '',
+            'is_on_bundoora_campus' => 'User is on Bundoora campus network',
+            'is_on_campus' => 'User is on campus',
+            'user_department' => 'User department',
+            'user_email_domain' => 'User email domain',
+            'user_institution' => 'User institution',
+            'user_name' => 'User name',
+            'user_profile_field_faculty_cost_code' => 'Faculty cost code user profile field',
+        );
+
+        $this->assertSame($expected, $actual);
+
+        $actual = dimensions::setting_options('action');
+        $expected = array (
+            '' => '',
+            'context' => 'Context',
+            'course_category_hierarchy_full_path' => 'Course category hierarchy full path',
+            'course_enrolment_method' => 'Course enrolment method',
+            'course_full_name' => 'Course full name',
+            'course_id_number' => 'Course ID number',
+            'course_short_name' => 'Course short name',
+            'user_role' => 'User role',
+        );
+
+        $this->assertSame($expected, $actual);
+    }
+
 }
